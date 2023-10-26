5136227e4b474ed98c24add093cee45dfaa108cb
Serhii Myshko <sergeiown@gmail.com>
2023-10-27 00:50:16 +0300

    create a main module

---
+++ main.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from pytube import YouTube
+
+# Function for downloading videos
+
+
+def download_video():
+    url = entry.get()
+    yt = YouTube(url)
+    stream = yt.streams.get_highest_resolution()
+    stream.download()
+    status_label.config(text="Відео завантажено!")
+
+
+# Creating a window
+window = tk.Tk()
+window.title("Завантажити відео з YouTube")
+
+# Label and field for entering a link
+label = tk.Label(window, text="Введіть посилання на відео YouTube:")
+label.pack()
+entry = tk.Entry(window)
+entry.pack()
+
+# Button for downloading
+download_button = tk.Button(window, text="Завантажити", command=download_video)
+download_button.pack()
+
+# Label for the status
+status_label = tk.Label(window, text="")
+status_label.pack()
+
+
+window.mainloop()
