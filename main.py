04a49307261c193689390790c3f374568b4bded2
Serhii Myshko <sergeiown@gmail.com>
2023-10-27 01:25:55 +0300

    change window size

--- main.py
+++ main.py
@@ -16,15 +16,27 @@ def download_video():
 window = tk.Tk()
 window.title("Завантажити відео з YouTube")
 
-# Label and field for entering a link
-label = tk.Label(window, text="Введіть посилання на відео YouTube:")
-label.pack()
-entry = tk.Entry(window)
-entry.pack()
+# Get the screen width and height
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+# Set the window size to 25% of the screen width and height
+window_width = int(screen_width * 0.25)
+window_height = int(screen_height * 0.25)
+window.geometry(f"{window_width}x{window_height}")
+
+# Label for entering a link
+label = tk.Label(
+    window, text="Введіть посилання на відео YouTube:", font=("Helvetica", 16))
+label.pack(pady=20)
+
+# Entry field for the link
+entry = tk.Entry(window, width=40)
+entry.pack(fill=tk.X, padx=20, pady=20)
 
 # Button for downloading
 download_button = tk.Button(window, text="Завантажити", command=download_video)
-download_button.pack()
+download_button.pack(pady=20)
 
 # Label for the status
 status_label = tk.Label(window, text="")
