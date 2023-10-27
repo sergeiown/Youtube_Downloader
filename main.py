31b3bd824493226932ab8deb9af369df02715d55
Serhii Myshko <sergeiown@gmail.com>
2023-10-27 05:20:11 +0300

    add the ability to select video quality

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import pyperclip
 import threading
 import os
 
+# Create a global variable to track the current progress bar
+current_progress_bar = None
+
 # Function to download the video
 
 
@@ -15,25 +18,41 @@ def download_video():
         status_label.config(text="Invalid link!")
         return
 
+    selected_quality = quality_var.get()
     download_thread = threading.Thread(
-        target=download_video_thread, args=(url,))
+        target=download_video_thread, args=(url, selected_quality))
     download_thread.start()
 
 # Function to download the video in a separate thread
 
 
-def download_video_thread(url):
+def download_video_thread(url, quality):
+    global current_progress_bar
+    # Check if a progress bar is already displayed and remove it
+    if current_progress_bar:
+        current_progress_bar.pack_forget()
+
     progress_bar = ttk.Progressbar(window, mode="indeterminate", length=400)
-    progress_bar.pack(pady=15)
+    progress_bar.pack(pady=30)
+    current_progress_bar = progress_bar  # Set the current progress bar
+
     yt = YouTube(url)
 
     try:
-        stream = yt.streams.get_highest_resolution()
+        if quality == "High Quality":
+            stream = yt.streams.get_highest_resolution()
+        elif quality == "Medium Quality":
+            stream = yt.streams.filter(res="720p").first()
+        elif quality == "Low Quality":
+            stream = yt.streams.filter(res="360p").first()
+
         status_label.config(text="Downloading in progress...")
         progress_bar.start()
         filename = get_valid_filename(yt.title)
+        filetypes = [("Video Files", (".mp4", ".avi", ".mkv")),
+                     ("All Files", "*.*")]
         save_path = filedialog.asksaveasfilename(
-            defaultextension=".mp4", initialfile=filename)
+            defaultextension=".mp4", initialfile=filename, filetypes=filetypes)
         if save_path:
             stream.download(output_path=os.path.dirname(
                 save_path), filename=os.path.basename(save_path))
@@ -47,7 +66,8 @@ def download_video_thread(url):
         error_label.config(text=str(e))
         window.after(5000, lambda: error_label.config(text=""))
     finally:
-        progress_bar.pack_forget()
+        if 'stream' in locals():
+            progress_bar.pack_forget()
 
 # Function to paste a link from the clipboard
 
@@ -91,19 +111,22 @@ def get_valid_filename(s):
 
 
 def update_buttons():
+    global current_progress_bar
     url = entry.get("1.0", tk.END).strip()
     if validators.url(url):
-        if not download_button.winfo_ismapped():
+        if not current_progress_bar:  # Only create a new progress bar if none is active
             download_button.pack(pady=15)
-        clear_button.pack(pady=5)  # Show the "Clear" button
+        clear_button.pack(pady=5)
         paste_button.pack_forget()
     else:
+        if current_progress_bar:
+            current_progress_bar.pack_forget()
+            current_progress_bar = None  # Reset the current progress bar
         if download_button.winfo_ismapped():
             download_button.pack_forget()
-        clear_button.pack_forget()  # Hide the "Clear" button
+        clear_button.pack_forget()
         paste_button.pack(pady=15)
 
-    # Set the maximum width for error messages and wrap text
     set_max_error_label_width()
 
 
@@ -115,29 +138,44 @@ window.title("Download Video from YouTube")
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
-# Set the window size to 25% of the screen width and height
+# Set the window size to 25% of the screen width and 35% height
 window_width = int(screen_width * 0.25)
-window_height = int(screen_height * 0.25)
+window_height = int(screen_height * 0.35)
 window.geometry(f"{window_width}x{window_height}")
+
+# Label for choosing video quality
+quality_label = tk.Label(
+    window, text="Choose video quality:", font=("Helvetica", 16))
+quality_label.pack(pady=10)
+
+# Options for video quality
+quality_options = ["High Quality", "Medium Quality", "Low Quality"]
+
+# Combobox for selecting video quality
+quality_var = tk.StringVar()
+quality_var.set(quality_options[0])
+quality_combobox = ttk.Combobox(
+    window, textvariable=quality_var, values=quality_options, width=25)
+quality_combobox.pack()
 
 # Label for entering the link
 label = tk.Label(
-    window, text="Paste a link to a YouTube video:", font=("Helvetica", 16))
+    window, text="Link to a YouTube video:", font=("Helvetica", 16))
 label.pack(pady=10)
 
 # Entry field for entering the link
-entry = tk.Text(window, height=1, width=40, state="disabled")
+entry = tk.Text(window, height=1, width=50, state="disabled")
 entry.pack(padx=10, pady=2)
 
 # Button to download
 download_button = tk.Button(
-    window, text="Download", command=download_video)
+    window, text="Download", command=download_video, width=25)
 download_button.pack_forget()
 
 # Button to clear the entry field
 clear_button = tk.Button(
-    window, text="Clear", command=clear_entry)
-clear_button.pack(pady=2)  # Adjust the padding as needed
+    window, text="Clear", command=clear_entry, width=25)
+clear_button.pack(pady=2)
 
 # Label for the status
 status_label = tk.Label(window, text="")
@@ -150,7 +188,7 @@ error_label.pack()
 
 # Button to paste a link from the clipboard
 paste_button = tk.Button(
-    window, text="Paste from clipboard", command=paste_from_clipboard)
+    window, text="Paste URL from clipboard", command=paste_from_clipboard, width=25)
 paste_button.pack(pady=15)
 
 # Update the state of buttons and the presence of the "Download" button at the beginning
